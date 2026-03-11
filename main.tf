711f5ef39419b260f1b21f6b43044e663cf9b0a6
Jeff <jeff8247@gmail.com>
2026-03-11 15:32:09 +1100

    Bump module to v1.0.4

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ check "datastore_exclusive" {
 }
 
 module "vm" {
-  source = "github.com/Jeff8247/module-vmware-virtual-machine?ref=v1.0.3"
+  source = "github.com/Jeff8247/module-vmware-virtual-machine?ref=v1.0.4"
 
   # Infrastructure placement
   datacenter        = var.datacenter
